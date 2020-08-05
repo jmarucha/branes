40a2293da06d2445e1c4402bf01e4085c6df7397
Jan K. Marucha <jkmarucha@gmail.com>
2020-08-05 17:12:13 +0200

    fix reruning issues

--- run_problems.py
+++ run_problems.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os  # os module allows to determine directories
 import subprocess
-from util import config, gen_header, is_running
+from util import config, gen_header, is_running, should_rerun
 
 sdpb_params = """\
 --procsPerNode={processes_per_node}
@@ -28,7 +28,6 @@ echo ending the job: `date`
 for name in fileNames:
     in_name     = name + '_in'
     out_name    = name +'_out'
-    out_full_dir = os.path.abspath(os.path.join(config['directories']['sdpb_output'],out_name))
     text       = job_template.format(
         header = gen_header(os.path.abspath(config['directories']['sdpb_binaries'])),
         nodes_per_job = config['sdpb']['nodes_per_job'],
@@ -38,10 +37,10 @@ for name in fileNames:
         sdpb_params = sdpb_params,
         sdpb = sdpb,
     )
-    if os.path.exists(os.path.join(out_full_dir, "out.txt")) and os.stat(os.path.join(out_full_dir, "out.txt")).st_size > 0:
-        print("Output of {} exists, skipping.".format(name))
-        continue 
+    
     sbatch_name = os.path.join(config['directories']['sdpb_binaries'], 'run_' + name + '.sh')
+    if config['cluster']['avoid_finished_jobs'] and not should_rerun(name):
+        continue
     if config['cluster']['avoid_repeated_jobs'] and is_running(sbatch_name):
         print('Task {} already running, skipping.'.format(os.path.basename(sbatch_name)))
         continue
--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import toml
 import os
 import json
 import subprocess
+import re
 
 config = toml.load('config.toml')
 
@@ -32,5 +33,30 @@ if not __name__ != "__main__" and config['cluster']['avoid_repeated_jobs']:
 def is_running(name):
     return os.path.basename(name) in current_jobs
 
+def should_rerun(name):
+    out_full_dir = os.path.abspath(os.path.join(config['directories']['sdpb_output'],name + '_out'))
+    if os.path.exists(os.path.join(out_full_dir, "out.txt")) and os.stat(os.path.join(out_full_dir, "out.txt")).st_size > 0:
+        with open(os.path.join(out_full_dir, 'out.txt')) as output:
+            terminate_reason = re.match('terminateReason = "(.+)";',output.readline())
+            if terminate_reason == "found primal-dual optimal solution":
+                print(f"Skipping, output of {name} exists, solution found")
+                return False
+            if terminate_reason == "maxComplementarity exceeded":
+                print(f"Skipping, output of {name} exists, problem is divergent")
+                return False
+
+
+            if terminate_reason == "maxIterations exceeded":
+                print(f"Rerunning, output of {name} exists, previous computation iterations number exceeded")
+                return True
+            if terminate_reason == "maxRuntime exceeded":
+                print(f"Rerunning, output of {name} exists, previous run exceeded runtime")
+                return True
+            print(f"Rerunning, output of {name} exists, but is weird...")
+            return True
+    else:
+        print(f"Brand new run of {name} inbound!")
+        return True
+            
 if __name__ == "__main__":
     print(json.dumps(config))
